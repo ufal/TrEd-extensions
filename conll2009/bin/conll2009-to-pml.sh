0e908f767952d0d71fd836909e6d484656af6029
Petr Pajas <pajas@ufal.mff.cuni.cz>
2009-04-19 12:18:38 +0000

    - use <conll2009> as root tag - create files with 50 trees per file

--- conll2009/bin/conll2009-to-pml.sh
+++ conll2009/bin/conll2009-to-pml.sh
@@ -11,7 +11,7 @@ if (( ! $# )) || [[ $1 == -h || $1 == --help ]] ; then
 fi
 
 btred=btred
-sentences_per_file=401
+sentences_per_file=50
 
 while (( $# )) ; do
     if [[ -f "$1" ]] ; then
@@ -20,7 +20,7 @@ while (( $# )) ; do
         ((max-=14))
         arglist=''
         for n in $(seq 1 $max) ; do arglist=$arglist,APRED_$n ;done
-        "$bin"/conll2pml -r -o "$1" -m $sentences_per_file -c ID,FORM,LEMMA,PLEMMA,POS,PPOS,FEAT,PFEAT,HEAD,PHEAD,DEPREL,PDEPREL,FILLPRED,PRED$arglist "$1"
+        "$bin"/conll2pml -R conll2009 -r -o "$1" -m $sentences_per_file -c ID,FORM,LEMMA,PLEMMA,POS,PPOS,FEAT,PFEAT,HEAD,PHEAD,DEPREL,PDEPREL,FILLPRED,PRED$arglist "$1"
         sed -i~ 's/\(<s:member name="apred_1">\)/<s:member name="apreds" type="feats.type"\/>\1/' "$1_schema.xml"
         "$btred" -S -I "$bin"/args.btred "$1"_*.pml
         "$bin"/remove_argN.sh "$1"_schema.xml
