08be317a070a030665d54431dd57d6a608a7ba85
Jan Štěpánek <jan.stepanek@matfyz.cz>
2010-12-16 18:48:34 +0000

    set env. variable(s)

--- release.sh
+++ release.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
+EXTDIR=`dirname $(readlink -fen $0)`
+. "$EXTDIR"/../admin/env.sh
+
+WWW_TRED="$WWW"/tred
+WWW_EXT=ufal:/home/pajas/WWW/tred/extensions/
 
 for ext ; do
-    rsync --exclude='*/.svn*' -avr /home/pajas/WWW/tred/extensions/{$ext.zip,$ext/,index.html} ufal:/home/pajas/WWW/tred/extensions/
+    rsync --exclude='*/.svn*' -avr "$WWW_TRED"/extensions/{$ext.zip,$ext/,index.html} "$WWW_EXT"
 done
