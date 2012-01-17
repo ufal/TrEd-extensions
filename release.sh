e1ed45c08cfb57b3951c69d8e09d69554e621136
Jan Štěpánek <jan.stepanek@matfyz.cz>
2012-01-17 14:52:00 +0000

    check the extension is in list to become public

--- release.sh
+++ release.sh
@@ -8,4 +8,6 @@ WWW_EXT=ufal:/home/pajas/WWW/tred/extensions/
 for ext ; do
     ext=${ext%/}
     rsync --exclude='*/.svn*' -avr "$WWW_TRED"/extensions/{$ext.zip,$ext,index.html} "$WWW_EXT"
+    wget http://ufal.ms.mff.cuni.cz/~pajas/tred/extensions/extensions.lst -O- | grep -q "^$ext$" || echo "PROBLEM: $ext missing in file \`extensions.lst' at the server!" >&2
 done
+
